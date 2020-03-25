29dd969dc2d18ed11cf7affb5d30327eaeef3679
Christine Augustine <blacchoy@gmail.com>
2020-03-25 14:28:04 -0700

    add js and new submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
+  $("form#height-form").submit(function(event) {
+    var feet = parseInt($("input#feet").val());
+    var inches = parseInt($("input#inches").val());
+    var totalHeight = (feet*12) + inches;
+    console.log(totalHeight)
+    $('.shadow').toggleClass('no-show')
+    $('.btn-danger').toggleClass('no-show')
+    if(totalHeight<= 60){
+      $('#rollercoaster3').toggleClass('no-show')
+    } else if(totalHeight > 60 && totalHeight < 72){
+      $('#rollercoaster3').toggleClass('no-show')
+      $('#rollercoaster2').toggleClass('no-show')
+    } else{
+      $('#rollercoaster3').toggleClass('no-show')
+      $('#rollercoaster2').toggleClass('no-show')
+      $('#rollercoaster1').toggleClass('no-show')
+    }
+    event.preventDefault();
+  });
+
+  $('.btn-danger').click(function(){
+    location.reload();
+  })
 
 });
